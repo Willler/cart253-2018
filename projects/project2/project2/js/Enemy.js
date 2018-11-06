6876d3b34be1e43fa9e3f05787291ea2f18ed3ab
William L'Eriger <espermentalout@gmail.com>
2018-11-06 16:10:34 -0500

    p2: added new class, included beginning comments

---
+++ projects/project2/project2/js/Enemy.js
@@ -0,0 +1,5 @@
+// Enemy
+//
+// A class to define how a new type of object, the enemy, behaves. Including bouncing on the top
+// and bottom edges of the canvas, going off the left and right sides,
+// and bouncing off paddles, reducing the player's score when hits
