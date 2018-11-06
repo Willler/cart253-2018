14cd8caaae694f09d4adf26f972cce69c2b5741d
William L'Eriger <espermentalout@gmail.com>
2018-11-06 01:52:01 -0500

    p2: made some changed to the paddles, changed size, added border radius

--- projects/project2/project2/js/Paddle.js
+++ projects/project2/project2/js/Paddle.js
@@ -6,13 +6,14 @@
 // Paddle constructor
 //
 // Sets the properties with the provided arguments or defaults
-function Paddle(x,y,w,h,speed,downKey,upKey) {
+function Paddle(x,y,w,h,border, speed,downKey,upKey) {
   this.x = x;
   this.y = y;
   this.vx = 0;
   this.vy = 0;
   this.w = w;
   this.h = h;
+  this.border = border; //////////////NEWWWWWWWW ******
   this.speed = speed;
   this.downKey = downKey;
   this.upKey = upKey;
@@ -45,7 +46,9 @@ Paddle.prototype.update = function() {
 // display()
 //
 // Draw the paddle as a rectangle on the screen
+// *******NEWWWWWWWW
 Paddle.prototype.display = function() {
-  fill(255);
-  rect(this.x,this.y,this.w,this.h);
+  stroke(244, 66, 209);
+  fill(56, 168, 255);
+  rect(this.x,this.y,this.w,this.h,this.border); ///////***********NEW*******
 }
--- projects/project2/project2/js/script.js
+++ projects/project2/project2/js/script.js
@@ -22,10 +22,10 @@ function setup() {
   // Create a ball
   ball = new Ball(width/2,height/2,5,5,10,5);
   // Create the right paddle with UP and DOWN as controls
-  rightPaddle = new Paddle(width-10,height/2,10,60,10,DOWN_ARROW,UP_ARROW);
+  rightPaddle = new Paddle(width-30,height/2,5,90,5,10,DOWN_ARROW,UP_ARROW); /////*****MODIFIED
   // Create the left paddle with W and S as controls
   // Keycodes 83 and 87 are W and S respectively
-  leftPaddle = new Paddle(0,height/2,10,60,10,83,87);
+  leftPaddle = new Paddle(20,height/2,5,90,5,10,83,87); ///****MODIFIED
 }
 
 // draw()
