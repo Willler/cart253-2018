5ba7b7f9fdb93c6efb65bab5790fd47afd8c643f
William L'Eriger <espermentalout@gmail.com>
2018-11-06 16:26:45 -0500

    p2: added isOffScreen function and collision function to the enemy

--- projects/project2/project2/js/Enemy.js
+++ projects/project2/project2/js/Enemy.js
@@ -35,3 +35,41 @@ Enemy.prototype.update = function () {
     this.vy = -this.vy;
   }
 }
+
+// isOffScreen()
+//
+// Checks if the ball has moved off the screen and, if so, wraps it to the other side, x axis
+Enemy.prototype.isOffScreen = function () {
+  // Check for going off screen and wrap if so
+  if (this.x + this.size < 0) {
+    this.x += width;
+  } else if (this.x > width) {
+    x -= width;
+  }
+}
+
+// display()
+//
+// Draw the Enemy as a rectangle on the screen
+Enemy.prototype.display = function () {
+  fill(244, 66, 209);
+  rect(this.x,this.y,this.size,this.size, this.border);
+}
+
+// handleCollision(paddle)
+//
+// Check if this enemy object overlaps the paddle passed as an argument
+// and if so reverse x velocity to bounce and take off a point from the player
+Enemy.prototype.handleCollision = function(paddle) {
+  // Check if the ball overlaps the paddle on x axis
+  if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
+    // Check if the ball overlaps the paddle on y axis
+    if (this.y + this.size > paddle.y && this.y < paddle.y + paddle.h) {
+      // If so, move ball back to previous position (by subtracting current velocity)
+      this.x -= this.vx;
+      this.y -= this.vy;
+      // Reverse x velocity to bounce
+      this.vx = -this.vx;
+    }
+  }
+}
