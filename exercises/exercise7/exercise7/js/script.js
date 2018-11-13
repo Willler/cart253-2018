81f491517f1a551dc1c744a3588c016a1d73633f
William L'Eriger <espermentalout@gmail.com>
2018-11-13 15:54:40 -0500

    ex7: drew basic face for prototype menu

--- exercises/exercise7/exercise7/js/script.js
+++ exercises/exercise7/exercise7/js/script.js
@@ -8,10 +8,31 @@
 // and text previously hidden in the background will get revealed within spotlight
 
 function setup() {
-  createCanvas(700,700);
+  createCanvas(1000,500);
   background(0);
 }
 
 function draw() {
+  drawMenuHead();
+}
 
+function drawMenuHead() {
+  push();
+
+  // the hat
+  ellipseMode(CENTER);
+  fill(255);
+
+  // the face
+  ellipse(width/2, 300, 130, 150);
+  fill(255,0,0);
+  noStroke();
+  ellipse(width/2, 250, 150, 150);
+
+  // the mouth
+  stroke(0);
+  strokeWeight(5);
+  line((width/2) - 10, 340, (width/2) + 10, 340);
+  
+  pop();
 }
