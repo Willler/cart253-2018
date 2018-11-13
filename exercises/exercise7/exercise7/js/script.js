9c125713811d7400eef5d0313c35b39762880402
William L'Eriger <espermentalout@gmail.com>
2018-11-13 15:26:18 -0500

    ex7: put in opening comments and the basics of the code

--- exercises/exercise7/exercise7/js/script.js
+++ exercises/exercise7/exercise7/js/script.js
@@ -1,1 +1,17 @@
+// Duality - The Mask and Reality
+// William L'Eriger
+//
+// A prototype of the Menu system I want to implement in my final project
+// An interactive menu that changes according to mouse location
+//
+// A floating, oscillating "head" in the middle of the canvas will shift to the left or right
+// and text previously hidden in the background will get revealed within spotlight
 
+function setup() {
+  createCanvas(700,700);
+  background(0);
+}
+
+function draw() {
+
+}
