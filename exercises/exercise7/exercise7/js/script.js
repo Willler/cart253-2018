bb3ad142711dbb916e9f4600f72a834307f04b34
William L'Eriger <espermentalout@gmail.com>
2018-11-13 16:18:53 -0500

    ex7: changed the menu face so that it oscillates, added comments to make everything clearer

--- exercises/exercise7/exercise7/js/script.js
+++ exercises/exercise7/exercise7/js/script.js
@@ -7,32 +7,60 @@
 // A floating, oscillating "head" in the middle of the canvas will shift to the left or right
 // and text previously hidden in the background will get revealed within spotlight
 
+
+// variables for oscillation
+var angle = 0;
+var radius = 150;
+
 function setup() {
   createCanvas(1000,500);
-  background(0);
+
 }
 
 function draw() {
   drawMenuHead();
 }
 
+
+// drawMenuHead
+//
+// where the oscillating face for the menu is drawn
 function drawMenuHead() {
+
+  // push content
   push();
+
+  // draw background
+  background(0);
+
+  // variable and calculation for oscillation, using a sine wave
+  var growth = sin(angle) * (radius/10);
+
+  // the shadow
+  ellipseMode(CENTER);
+  fill(25);
+  ellipse((width/2) + 10, 300, 130 + growth, 150 + growth);
+  noStroke();
+  ellipse((width/2) + 10, height/2, 150 + growth);
 
   // the hat
   ellipseMode(CENTER);
   fill(255);
 
   // the face
-  ellipse(width/2, 300, 130, 150);
+  ellipse(width/2, 300, 130 + growth, 150 + growth);
   fill(255,0,0);
   noStroke();
-  ellipse(width/2, 250, 150, 150);
+  ellipse(width/2, height/2, 150 + growth);
 
   // the mouth
-  stroke(0);
-  strokeWeight(5);
-  line((width/2) - 10, 340, (width/2) + 10, 340);
-  
+  // stroke(0);
+  // strokeWeight(5);
+  // line(((width/2) - 10) + growth, 340, (width/2) + 10, 340);
+
+  // the angle of the curve, which determines how fast everything will oscillate
+  angle += 0.03;
+
+  // pop contents
   pop();
 }
