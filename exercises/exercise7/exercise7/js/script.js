cf11178e4b467bd5475fd7321cf86104c9a812ce
William L'Eriger <espermentalout@gmail.com>
2018-11-14 00:30:58 -0500

    ex7: added vines opacity by plugging a variable in the alpha of the fill(), added a bunch of comments to make everything extra clear
    my biggest commit yet, yikes

--- exercises/exercise7/exercise7/js/script.js
+++ exercises/exercise7/exercise7/js/script.js
@@ -18,8 +18,9 @@ var headX;
 var headY;
 var headVX;
 
-// variables for constraining head position on the x axis
-var headMaxX
+// variable for opacity of vines, so that they appear gradually
+var vinesOpacity;
+var vinesOpacityGrowth;
 
 // preload()
 //
@@ -33,6 +34,8 @@ function preload() {
 
 function setup() {
   createCanvas(1000,500);
+
+  // define head location on both axes
   headX = width/2;
   headY = height/2;
 
@@ -42,19 +45,31 @@ function setup() {
   // play the background menuMusic
   menuMusic.play();
   menuMusic.loop = true;
+
+  // defining vines opacity and growth, which will be called in a later function
+  vinesOpacity = 0;
+  vinesOpacityGrowth = 1;
 }
 
 function draw() {
+  // draw the head
   drawMenuHead();
+
+  // call the spotlight
   spotlightDisplay();
+
+  // call the spotlight text
   spotlightText();
+
+  // call the spotlight sound effect
   playSpotlightSound();
 }
 
 
 // drawMenuHead
 //
-// where the oscillating face for the menu is drawn
+// where the oscillating head for the menu is drawn
+// for now it looks like a mushroom, so lets say its an expressionless person with a bowl cut (I plan to have diff expressions in final version)
 function drawMenuHead() {
 
   // push content
@@ -63,6 +78,7 @@ function drawMenuHead() {
   // draw background
   background(57, 77, 0);
 
+  // calling the drawSpotlightVines() here so that it shows above background, but below head image
   drawSpotlightVines();
 
   // determine velocity of the head image
@@ -84,7 +100,7 @@ function drawMenuHead() {
   fill(204, 153, 102);
   ellipse(headX, headY + 50, 130 + growth, 150 + growth);
 
-  // the hat
+  // the bowl cut
   fill(150,0,0);
   noStroke();
   ellipse(headX, headY, 150 + growth);
@@ -116,6 +132,11 @@ function headMovement() {
   headX = constrain(headX, 200, width - 200);
 }
 
+
+///////////////////////////////////// NOTE: the following functions all have the same (or similar) if statements
+///////////////////////////////////// but they are separated for clarity
+
+
 // spotlightDisplay();
 //
 // shows a spotlight/window where text will appear once the head reaches a certain point
@@ -125,17 +146,27 @@ function spotlightDisplay() {
 
   if (headX === 200) {
     // vx = 0;
+    //shadow
     fill(25);
     ellipse(660, height/2 + 10, 500 , 300);
-    fill(255);
+    //spotlight
+    fill(204, 153, 102);
     ellipse(650, height/2, 500 , 300);
   } else if (headX === (width - 200)) {
     // vx = 0;
-    fill(255);
+    //shadow
+    fill(25);
+    ellipse(340, height/2 + 10, 500, 300);
+    //spotlight
+    fill(204, 153, 102);
     ellipse(350, height/2, 500, 300);
   }
 }
 
+
+//spotlightText()
+//
+// function that shows the spotlight text when called
 function spotlightText() {
 
 // how the text will look
@@ -147,11 +178,15 @@ function spotlightText() {
 
 // if statement to show text when constrain extremities are shown
   if (headX === 200) {
+    //heading
     text("The Mask", 650, height/2);
+    //flavor
     textSize(24);
     text("- The Side Seen in Public -", 650, (height/2) + 50);
   } else if (headX === (width - 200)) {
+    //heading
     text("The Truth", 350, height/2);
+    //flavor
     textSize(24);
     text("- The Side Seen by None -", 350, (height/2) + 50);
   }
@@ -160,16 +195,30 @@ function spotlightText() {
 // drawSpotlightVines()
 //
 // draws a background vine-like display using enlarged letters when head stops moving
+// they start off with low opacity, which increases the more frames the head spends beyond a certain point
 function drawSpotlightVines() {
 
   noStroke();
 
-  if (headX === 200) {
+  // what determines the opacity increase
+  vinesOpacity = vinesOpacityGrowth += 0.8;
+  ///// vinesOpacity = constrain(vinesOpacity, 0, 100);
+
+  // fill the vines a red color, with an updating opacity
+  fill(150, 0, 0, vinesOpacity);
+
+  // if statement to see if headX is beyond certain points on the x-axis and start showing vines then
+  if (headX <= 400) {
     textSize(1500);
     text("FV", 400, 800);
-  } else if (headX === (width - 200)) {
+  } else if (headX >= (width - 400)) {
     textSize(1500);
     text("QS", 150, 800);
+  }
+
+  // if statement that resets the opacity growth to zero if user goes on the other side
+  if (headX === width/2) {
+    vinesOpacityGrowth = 0;
   }
 
 }
@@ -177,6 +226,7 @@ function drawSpotlightVines() {
 // playSpotlightSound()
 //
 // plays the spotlight guitar strum once head stops moving
+// there is currently an issue where the sound plays repeatedly, will fix that in final version
 function playSpotlightSound() {
   if (headX === 200 || headX === (width - 200)) {
     menuSpotlightSound.play();
