f789754db29ebc2cce9a4ea3894aaa2feff25b03
William L'Eriger <espermentalout@gmail.com>
2018-11-20 14:48:20 -0500

    ex8: added the groundwork for functions for visual elements in script.js, added the framework for the rain object class

---
+++ exercises/exercise8/exercise8/js/rain.js
@@ -0,0 +1,42 @@
+// A new class to create the rain, which will come down randomly
+// from the top of the canvas and fall down to the bottom
+// where the player has to intercept them
+
+
+// rain
+//
+// Where the rain object parameters and variables will be configured
+// and initialized
+function Rain() {
+
+}
+
+// update()
+//
+// where changes to the rain object are determined
+Rain.prototype.update = function () {
+
+}
+
+// touchedBottom()
+//
+// function that makes the rain drop do something when they reach the bottom
+// of the canvas
+Rain.prototype.touchedBottom = function () {
+
+}
+
+// display()
+//
+// where we determine the object's appearance
+Rain.prototype.display = function () {
+
+}
+
+// handleCollision()
+//
+// determines what happens when the rain collides with another object
+// in this case, it will be against the umbrella
+Rain.prototype.handleCollision = function () {
+
+}
--- exercises/exercise8/exercise8/js/script.js
+++ exercises/exercise8/exercise8/js/script.js
@@ -1,17 +1,18 @@
 /*****************
 
-Title of Project
-Author Name
+Duality - The Mask and Reality
+ William L'Eriger
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+
+A prototype of one of the minigames I will be putting into my final project
+Here, the player has to hold up their "mask", which in this case is an umbrella
+And shield themselves against the world/rain
 
 ******************/
 
 // preload()
 //
 // Description of preload
-
 function preload() {
 
 }
@@ -20,7 +21,6 @@ function preload() {
 // setup()
 //
 // Description of setup
-
 function setup() {
 
 }
@@ -29,7 +29,14 @@ function setup() {
 // draw()
 //
 // Description of draw()
-
 function draw() {
 
+}
+
+function drawPlayer() {
+
+}
+
+function drawBackground() {
+  
 }
