9c0313cbbbdc335d66a04251f13fefcc6a8d7faa
William L'Eriger <espermentalout@gmail.com>
2018-11-20 14:53:32 -0500

    ex8: put in the groundwork for the umbrella object class in a separate js file

---
+++ exercises/exercise8/exercise8/js/umbrella.js
@@ -0,0 +1,34 @@
+// A new class to create the umbrella, which shields the player against
+// the atrocities of the world(or rain)
+// a symbol for the "mask" we wear in public
+
+
+// umbrella()
+//
+// Where the umbrella object parameters and variables will be configured
+// and initialized
+function Umbrella() {
+
+}
+
+// update()
+//
+// where changes to the umbrella object are determined
+Umbrella.prototype.update = function () {
+
+}
+
+// handleInput()
+//
+// function that determines how the umbrella will be controlled
+// by the player, such as movement
+Umbrella.prototype.handleInput = function () {
+
+}
+
+// display()
+//
+// where we determine the object's appearance
+Umbrella.prototype.display = function () {
+
+}
