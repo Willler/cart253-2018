e977a4cfac5a71b9da0c7e4a212aa5909310516f
William L'Eriger <espermentalout@gmail.com>
2018-11-27 13:49:42 -0500

    ex8.1: trying out new link

---
+++ exercises/exercise8_1/exercise8_1/js/Umbrella.js
@@ -0,0 +1,72 @@
+// A new class to create the umbrella, which shields the player against
+// the atrocities of the world(or rain)
+// a symbol for the "mask" we wear in public
+
+
+// umbrella()
+//
+// Where the umbrella object parameters and variables will be configured
+// and initialized
+function Umbrella(x, y, width, height, speed, rightKey, leftKey) {
+  this.x = x;
+  this.y = y;
+  this.vx = 0;
+  this.vy = 0;
+  this.width = width;
+  this.height = height;
+  this.speed = speed;
+  this.rightKey = rightKey;
+  this.leftKey = leftKey;
+}
+
+// update()
+//
+// where changes to the umbrella object are determined
+Umbrella.prototype.update = function () {
+  this.x += this.vx;
+  // this.x = constrain(this.x, 50, width - 50);
+}
+
+// handleInput()
+//
+// function that determines how the umbrella will be controlled
+// by the player, such as movement, check if right or left arrow key is pressed
+Umbrella.prototype.handleInput = function () {
+
+  // if the appropriate key is down, move left or right, if nothing pressed stay still
+  if (keyIsDown(this.rightKey)) {
+    this.vx = this.speed;
+}
+  else if (keyIsDown(this.leftKey)) {
+    this.vx = -this.speed;
+}
+  else {
+    this.vx = 0;
+}
+}
+
+// display()
+//
+// where we determine the object's appearance
+// it is a bright red umbrella to contrast the rest of the elements
+Umbrella.prototype.display = function () {
+
+  noStroke();
+  rectMode(CENTER);
+
+  // frills and decorations on the bottom
+  fill(150, 0, 0);
+  ellipse(this.x, this.y + 25, this.width/3, this.height/2);
+  ellipse(this.x - 40, this.y + 15, this.width/3, this.height);
+  ellipse(this.x - 60, this.y + 20, this.width/4, this.height +10);
+  ellipse(this.x + 40, this.y + 15, this.width/3, this.height);
+  ellipse(this.x + 60, this.y + 20, this.width/4, this.height +10);
+
+  fill(200, 0, 0);
+  // the main object which raindrops interact with
+  ellipse(this.x, this.y, this.width, this.height);
+
+  fill(0);
+  rect(this.x, this.y - 10, 5, 10, 3);
+
+}
---
+++ exercises/exercise8_1/exercise8_1/js/script.js
@@ -0,0 +1,226 @@
+/*****************
+
+Duality - The Mask and Reality
+ William L'Eriger
+
+
+A prototype of one of the minigames I will be putting into my final project
+Here, the player has to hold up their "mask", which in this case is an umbrella
+And shield themselves against the world/rain
+
+******************/
+
+// player character variables
+var playerX;
+var playerY;
+var playerVX;
+var playerVY;
+
+// variables for rain array
+var rain = [];
+var rainDrops = 50;
+
+// variables for the Umbrella
+var umbrella;
+
+// preload()
+//
+// Description of preload
+function preload() {
+  rainSound = new Audio("assets/sounds/rain.wav");
+  rainBackgroundMusic = new Audio("assets/sounds/loneliness.wav");
+
+  promptFont = loadFont("assets/fonts/display.ttf");
+}
+
+
+// setup()
+//
+// Description of setup
+function setup() {
+
+  //creating the canvas
+  createCanvas(1000,500);
+
+  //determine the initial player character position
+  playerX = width/2;
+  playerY = height - 50;
+
+  // initialize value of playerVX
+  playerVX = 2;
+
+  // create the rain object array
+  for (var i = 0; i < rainDrops; i++) {
+  rain.push(new Rain(random(50,950),0,0,random(3,5),6,5,5));
+  }
+
+  // create the umbrella object
+  umbrella = new Umbrella(width/2, 300, 150, 50, 5, RIGHT_ARROW, LEFT_ARROW);
+
+  // setup the background music and rain sounds
+  rainSound.play();
+  rainSound.loop = true;
+  rainBackgroundMusic.play();
+  rainBackgroundMusic.loop = true;
+
+}
+
+
+// draw()
+//
+// Description of draw()
+function draw() {
+  // background functions
+  drawBackground();
+  drawBackgroundText()
+  drawPlayer();
+
+  // as the name implies
+  playerMovement();
+
+  // functions taken from the umbrella script
+  umbrella.handleInput();
+  umbrella.update();
+  umbrella.display();
+
+ // call the rain.js functions through the array
+  for (var i = 0; i < rain.length; i++) {
+    rain[i].update();
+    rain[i].touchedBottom();
+    rain[i].display();
+    rain[i].handleCollision(umbrella);
+  }
+
+
+}
+
+// drawPlayer()
+//
+// function to draw the player sprite
+// it is grey/monochrome on purpose
+// to show contrast between the colorful mask/umbrella and the reality underneath
+function drawPlayer() {
+
+  //hair background
+  fill(25);
+  ellipse(playerX, playerY + 10, 55, 50);
+  ellipse(playerX - 22, playerY + 15, 20, 30);
+  ellipse(playerX + 22, playerY + 15, 20, 30);
+
+  //face
+  fill(150);
+  ellipse(playerX, playerY, 50);
+
+  //glasses
+  stroke(0);
+  noFill();
+  ellipse(playerX - 9, playerY + 10, 15);
+  ellipse(playerX + 9, playerY + 10, 15);
+
+
+  //hair foreground
+  noStroke();
+  fill(45);
+  ellipse(playerX + 25, playerY + 5, 20);
+  ellipse(playerX + 15, playerY - 5, 30);
+  ellipse(playerX - 25, playerY + 5, 20);
+  ellipse(playerX - 15, playerY - 5, 30);
+  ellipse(playerX + 2, playerY - 12, 40, 30);
+
+}
+
+// playerMovement()
+//
+// function to determine how the player sprite moves around
+// for now, it moves left to right statically, will improve with perlin noise or something better suited in final version
+function playerMovement() {
+  playerX = playerX + playerVX;
+
+  // constrain the player within set boundaries on the x-axis
+  if (playerX === 150 || playerX === 850) {
+    playerVX = -playerVX;
+  }
+}
+
+// drawBackground()
+//
+// draw the background image
+// an evening cityscape, melancholy and dreary
+function drawBackground() {
+  background(11, 18, 30, 80);
+
+  //city hue/mist
+  ellipseMode(CENTER);
+  noStroke();
+  fill(66, 58, 89);
+  ellipse(width/2, height, width + 50, height + 50, 80);
+  fill(44, 38, 61);
+  ellipse(width/2, height, width, height, 80);
+
+
+  //buildings (there is probably a better way to do this), with lights put in here and there
+  fill(0);
+  rectMode(CORNER);
+
+  // first three buildings on the left
+  rect(0, 200, 75, 300);
+  fill(255, 255, 0, 80);
+  ellipse(60, 300, 20);
+  fill(255, 255, 0, 40);
+  ellipse(60, 300, 40);
+  fill(255, 255, 0, 10);
+  ellipse(60, 300, 60);
+  fill(0);
+  rect(180, 150, 75, 350);
+  rect(60, 100, 125, 400);
+
+  // buildings in the middle
+  rect(300, 125, 60, 375);
+  rect(470, 200, 120, 300);
+  //light
+  fill(255, 255, 0, 80);
+  ellipse(590, 270, 20);
+  fill(255, 255, 0, 40);
+  ellipse(590, 270, 40);
+  fill(255, 255, 0, 10);
+  ellipse(590, 270, 60);
+  fill(0);
+  //end light
+  rect(380, 180, 100, 320);
+  //light
+  fill(255, 255, 0, 80);
+  ellipse(390, 400, 20);
+  fill(255, 255, 0, 40);
+  ellipse(390, 400, 40);
+  fill(255, 255, 0, 10);
+  ellipse(390, 400, 60);
+  fill(0);
+  //end light
+  rect(590, 250, 150, 250);
+
+  // buildings on the right
+  rect(750, 50, 100, 450);
+  rect(840, 200, 75, 300);
+  //light
+  fill(255, 255, 0, 80);
+  ellipse(830, 150, 20);
+  fill(255, 255, 0, 40);
+  ellipse(830, 150, 40);
+  fill(255, 255, 0, 10);
+  ellipse(830, 150, 60);
+  fill(0);
+  //end light
+  rect(910, 100, 90, 400);
+}
+
+// drawBackgroundText()
+//
+// put in a text display to subtly show the player the controls
+// its not obvious, which is on purpose
+// as its not always easy to know what to do in public
+function drawBackgroundText() {
+  textSize(24);
+  fill(255);
+  textFont(promptFont);
+  text("<--   Brandish Your Mask   -->", 50, 50);
+}
